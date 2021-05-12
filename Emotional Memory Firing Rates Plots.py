31e4d7cf5ca2c8aca4873d7420cfb98b0f347982
Panagiota Loizidou <panloizidou@gmail.com>
2021-05-12 23:23:54 +0200

    Update Emotional Memory Firing Rates Plots.py

--- Emotional Memory Firing Rates Plots.py
+++ Emotional Memory Firing Rates Plots.py
@@ -1,7 +1,9 @@
 
-''' This is file that can be run to obtain different comparisons across brain states for three different kinds of neurons. 
+''' This file can be run to obtain different comparisons across brain states for three different kinds of neurons. 
 The user can choose the sessions of interest (sessions are represented by local directories with the files associated with each session), 
-two brain states among 'sws', 'wake','Rem', and 'drowsy', and finally a brain structure among 'Hpc' and 'BLA'''
+two brain states among the following:'sws', 'wake','Rem', and 'drowsy', and finally a brain structure among: 'Hpc' and 'BLA
+
+When the file is ran, the plots comparing BLA neural activity during wake, REM, and NREM sleep should be obtained.'''
 
 import firingratefunctions as fr
 import pickle
@@ -9,10 +11,10 @@ import pickle
 with open('\sessions.pkl', 'rb') as f:
     session_paths = pickle.load(f)
 
-fr.plot_scatter(session_paths, ['wake','sws'], 'Hpc')
+fr.plot_scatter(session_paths, ['wake','sws'], 'BLA')
 
-fr.plot_scatter(session_paths, ['wake','Rem'], 'Hpc')
+fr.plot_scatter(session_paths, ['wake','Rem'], 'BLA')
 
-fr.plot_both_histograms(session_paths, ['wake','sws'], 'Hpc',bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1)
+fr.plot_both_histograms(session_paths, ['wake','sws'], 'BLA',bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1)
 
-fr.plot_both_histograms(session_paths, ['wake','Rem'], 'Hpc',bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1)
+fr.plot_both_histograms(session_paths, ['wake','Rem'], 'BLA',bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1)
