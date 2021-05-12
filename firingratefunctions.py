9c6cca5addd29fed35908fdc4f9d5cef9b7f2413
Panagiota Loizidou <panloizidou@gmail.com>
2021-05-12 20:36:15 +0200

    variable names

--- firingratefunctions.py
+++ firingratefunctions.py
@@ -3,17 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import bk.load
 
-def split_neurons_types_regions(path, brain_region):
-    bk.load.current_session(path)
-    neurons, metadata = bk.load.loadSpikeData(path)
+def categorize_types_regions(session, brain_region):
+    bk.load.current_session(session)
+    neurons, metadata = bk.load.loadSpikeData(session)
     pyramidal = neurons[(metadata['Type']=='Pyr') & (metadata['Region']==brain_region)]
     interneurons = neurons[(metadata['Type']=='Int') & (metadata['Region']==brain_region)]
     other_neurons = neurons[metadata['Type']=='Unk']
     return pyramidal, interneurons, other_neurons
 
-def get_firing_rate_for_specific_state(neurons, state):
+def calculate_firing_rate_per_state(neurons, state):
     """
-    Returns a pandas Series object with the mean firing rate of each of the neurons during a chosen state (REM, sws, drowsy, or wake)
+    Returns a pandas Series with the mean firing rate of each of the neurons during a chosen state (REM, sws, drowsy, or wake)
     
     Keyword arguments:
     neurons -- numpy.ndarray containing series data of selected neurons 
@@ -34,41 +34,41 @@ def get_firing_rate_for_specific_state(neurons, state):
     return mean_firing_rates
 
 
-def get_FR_per_state_neurontype(path, states, brain_region):
+def calculate_firing_rate_per_state_per_type(session, states, brain_region):
     '''
     Returns 2 lists (one for each state) with the mean firing rate of each of the types of neurons (Pyramidal, interneurons, unknown) during the chosen state.
     
     Keyword arguments:
-    path -- string containing the dircetory where the data files are stored
+    session -- string containing the dircetory where the data files are stored
     states -- list of 2 strings, should be one of the following: REM, sws, drowsy, wake (default='REM')
     '''
-    neurons_by_type=split_neurons_types_regions(path, brain_region)
+    neurons_by_type=categorize_types_regions(session, brain_region)
     firing_rates_state0=[]
     firing_rates_state1=[]
     for i in neurons_by_type:
-        x=get_firing_rate_for_specific_state(i, states[0])
-        y=get_firing_rate_for_specific_state(i, states[1])
-        firing_rates_state0.append(x)
-        firing_rates_state1.append(y)
+        state0=calculate_firing_rate_per_state(i, states[0])
+        state1=calculate_firing_rate_per_state(i, states[1])
+        firing_rates_state0.append(state0)
+        firing_rates_state1.append(state1)
     return firing_rates_state0, firing_rates_state1
 
 
-def get_firing_rates_all_sessions(paths, states, brain_region):
+def calculate_firing_rates_multiple_sessions(sessions, states, brain_region):
     '''
     Returns N numpy arrays (one for each state) with the mean firing rate of each of the types of neurons (Pyramidal, interneurons, unknown) during the chosen state
     where N is the number of sessions.
     Returned lists structure = Number of sessions * number of types of cells(=3) * number of cells(depends on session)
     
     Keyword arguments:
-    paths -- list of strings containing the dircetories where the data files for each sessions are stored
+    sessions -- list of strings containing the dircetories where the data files for each sessions are stored
     states -- list of 2 strings, should be one of the following: REM, sws, drowsy, wake (default='REM')
     '''
     
     all_sessions_state0=[]
     all_sessions_state1=[]
 
-    for path in paths:
-        FR_per_cell_state0,FR_per_cell_state1=get_FR_per_state_neurontype(path, states, brain_region)
+    for session in sessions:
+        FR_per_cell_state0,FR_per_cell_state1=calculate_firing_rate_per_state_per_type(session, states, brain_region)
         all_sessions_state0.append(FR_per_cell_state0)
         all_sessions_state1.append(FR_per_cell_state1)
     return np.array(all_sessions_state0,dtype=object), np.array(all_sessions_state1,dtype=object)
@@ -80,14 +80,14 @@ def double_flatten(array):
     return flat_2
 
 
-def plot_scatter(paths, states, brain_region):
-    x,y=get_firing_rates_all_sessions(paths, states, brain_region)
+def plot_scatter(sessions, states, brain_region):
+    state0,state1=calculate_firing_rates_multiple_sessions(sessions, states, brain_region)
     identity=np.linspace(-10,100,101) 
     plt.plot(identity,identity, 'k-', color='b')
-    for i in range(len(x)):
-        plt.scatter(x[i][2],y[i][2], color='gray',alpha=0.5)
-        plt.scatter(x[i][0],y[i][0], color='r', alpha=0.5)
-        plt.scatter(x[i][1],y[i][1], color='b', alpha=0.5)
+    for i in range(len(state0)):
+        plt.scatter(state0[i][2],state1[i][2], color='gray',alpha=0.5)
+        plt.scatter(state0[i][0],state1[i][0], color='r', alpha=0.5)
+        plt.scatter(state0[i][1],state1[i][1], color='b', alpha=0.5)
     plt.yscale('log')
     plt.xscale('log')
     plt.xlabel(f'{states[0]} rate (Hz)')
@@ -115,8 +115,8 @@ def plot_histogram(data, title, bin_size=50, upper_axis_bound=100, lower_axis_bo
 
 
 
-def plot_both_histograms(paths, states, brain_region,bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1):
-    x,y=get_firing_rates_all_sessions(paths, states, brain_region)
-    histogram_region_1=plot_histogram(x, title=brain_region+ states[0])
-    histogram_region_2=plot_histogram(y, title=brain_region+ states[1])
+def plot_both_histograms(sessions, states, brain_region,bin_size=50, upper_axis_bound=100, lower_axis_bound=0.1):
+    state0,state1=calculate_firing_rates_multiple_sessions(sessions, states, brain_region)
+    histogram_region_1=plot_histogram(state0, title=brain_region+ states[0])
+    histogram_region_2=plot_histogram(state1, title=brain_region+ states[1])
 
